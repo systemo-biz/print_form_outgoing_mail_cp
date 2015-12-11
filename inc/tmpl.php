ed361362e67fd89bec63e1b8dee0f94b1b7b320c
Anatoly Yumashev <yumashev@users.noreply.github.com>
2015-12-11 18:17:42 +0500

    Update tmpl.php

--- inc/tmpl.php
+++ inc/tmpl.php
@@ -28,7 +28,7 @@
 
     <div id="content" class="clearfix">
         <?php if ( have_posts() ) while ( have_posts() ) : the_post(); $post = get_post(); ?>
-        
+
             <div id="print_wrapper" class="container">
                 <p id="print"><a href="javascript:window.print()">Печать</a></p>
                 <hr>
@@ -36,7 +36,7 @@
                     <div class="our_company_data col-xs-6">
                         <img src="<?php echo plugins_url( 'logo.png', __FILE__ ); ?>" style="width: 200px;">
                         <div><small>Сайт: <a href="http://systemo.biz">systemo.biz</a></small></div>
-                        <div><small>Телефон: <a href="tel:+73452602654">+7 (3452) 602654</a></small></div>
+                        <div><small>Телефон: <a href="tel:+78002008812">+7 (800) 200 8812</a></small></div>
                         <div><small>Электронная почта: <a href="email:s@systemo.biz">s@systemo.biz</a></small></div>
                         <div><small>Почтовый адрес: <br/>625039, г. Тюмень, а/я 6490</small></div>
                         <div class="reg_num"><small>Исх. № <?php the_ID(); ?> от <?php  the_date() ?></small></div>
@@ -44,9 +44,9 @@
                     <div class="client_wrapper col-xs-6">
                         <strong>Адресат:</strong>
                         <?php
-                            $reporter = get_post_meta($post->ID, 'reporter', true);
-                            $reporter = get_post($reporter[0]);
-                            
+                            $reporter = get_post_meta($post->ID, 'cp_to', true);
+                            $reporter = get_post($reporter);
+
                         ?>
                         <br />
                         <div>
@@ -71,7 +71,7 @@
                 </footer>
                 <hr/>
             </div>
-        
+
         <?php endwhile; ?>
     </div>
 </body>
